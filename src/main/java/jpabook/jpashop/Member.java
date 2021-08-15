4dbb60abd346174d9638d937437339b6e464ce35
doctorbakk <pju1580@gmail.com>
2021-08-16 00:24:39 +0900

    JPA+DB 설정 확인

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+}
